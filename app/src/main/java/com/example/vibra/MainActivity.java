f92acb076dfcfbb335073ca3f990bb8aa143fb25
homkim <homkim01@gmail.com>
2020-08-08 12:07:28 +0900

    다른 버튼 누르면 기존 진동 취소 기능 추

--- app/src/main/java/com/example/vibra/MainActivity.java
+++ app/src/main/java/com/example/vibra/MainActivity.java
@@ -27,12 +27,43 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void onVibButtonClicked(View v){
+    public void onButton1Clicked(View v){
         Toast.makeText(this, "1초 진동을 울립니다.",Toast.LENGTH_LONG).show();
 
         Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.cancel();
         vibrator.vibrate(1000); // 1초간 진동
     }
 
+
+    public void onButton2Clicked(View v){
+        Toast.makeText(this, "패턴진동을 울립니다.",Toast.LENGTH_LONG).show();
+
+        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.cancel();
+        long[] pattern = {100,300,100,700,300,2000}; // miliSecond
+        //           대기,진동,대기,진동,....
+        // 짝수 인덱스 : 대기시간
+        // 홀수 인덱스 : 진동시간
+        vibrator.vibrate(pattern, // 진동 패턴을 배열로
+                -1);     // 반복 인덱스
+        // 0 : 무한반복, -1: 반복없음,
+        // 양의정수 : 진동패턴배열의 해당 인덱스부터 진동 무한반복
+    }
+
+    public void onButton3Clicked(View v){
+        Toast.makeText(this, "무한진동을 울립니다.",Toast.LENGTH_LONG).show();
+
+        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.cancel();
+        vibrator.vibrate( new long[]{100,1000,100,500,100,500,100,1000} , 0);
+    }
+
+    public void onCancelClicked(View v){
+        Toast.makeText(this, "진동을 취소합니다.",Toast.LENGTH_LONG).show();
+
+        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.cancel();
+    }
 
 }
